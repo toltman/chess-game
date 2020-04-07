f07dc26905b9ff042a1952243da6678c9cb7e4af
Tim Oltman <toltman@gmail.com>
2020-04-07 08:51:55 -0400

    display board

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
-const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const board = document.getElementById("chess-board");
+
+// Maps letters onto numbers
+const alphaVal = (s) => s.toLowerCase().charCodeAt(0) - 97 + 1;
+
+// Get square color
+const getSquareColor = (row, col) =>
+  (row + alphaVal(col)) % 2 == 0 ? "white" : "black";
 
 for (let row = 0; row < 8; row++) {
-    for (let col of cols) {
-        let htmlSquare = document.createElement('div')
-        htmlSquare.setAttribute('id', `${col}${row}`)
-    }
+  let rowDiv = document.createElement("div");
+  rowDiv.setAttribute("class", "row");
+  board.appendChild(rowDiv);
+
+  for (let col of cols) {
+    let htmlSquare = document.createElement("div");
+    htmlSquare.id = `${col}${row + 1}`;
+    htmlSquare.className = `square ${getSquareColor(row, col)}`;
+    rowDiv.appendChild(htmlSquare);
+  }
 }
